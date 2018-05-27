74850339d1b0945ecfa248e8069061f33320e59e
Samuel Betio <38454185+lmas2969@users.noreply.github.com>
2018-05-27 18:13:30 +0800

    Revert "Rich content editing entirely re-imagined for the web"

--- spec/javascripts/units/format_bar.spec.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-describe("FormatBar", function(){
-});
--- src/packages/uuid.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// jshint ignore: start
-module.exports = function uuid(a,b){
-  for(b=a='';a++<36;b+=a*51&52?(a^15?8^Math.random()*(a^20?16:4):4).toString(16):'-');
-  return b;
-};
-// jshint ignore: end
