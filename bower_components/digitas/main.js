1de7ba59a5f56d333b551f3d5357445eaaba47b6
Samuel Rapana Betio <samuelbetio@users.noreply.github.com>
2018-06-29 14:27:35 +0800

    document.getElementById dayrikta sa link
    document.getElementById("topLeft").onclick=contact;
    document.getElementById('topMid').onclick=skin;
    document.getElementById('topRight').onclick=skinCare
    document.getElementById('bottomLeft').onclick=sunscreen
    document.getElementById('bottomMid').onclick=creme
    document.getElementById('bottomRight').onclick=secret
    function contact() {
      var communication = window.open('https://www.nivea.in/about-us/contact')
    }
    function skin() {
      var type = window.open('https://www.nivea.in/advice/nice-skin/what-skin-type-are-you-190')
    }
    function skinCare() {
      var help = window.open('https://www.nivea.in/advice/nice-skin/what-skincare-type-are-you-160')
    }
    function sunscreen() {
      var liquid = window.open('https://www.nivea.in/advice/sun-protection/the-right-sun-cream-102')
    }
    function creme() {
      var different = window.open('https://www.nivea.in/shop/creams')
    }
    function secret() {
      var discovery = window.open('https://www.nivea.in/advice/nice-skin/q10-q10plus-200')
    }

---
+++ bower_components/digitas/main.js
@@ -0,0 +1,24 @@
+document.getElementById("topLeft").onclick=contact;
+document.getElementById('topMid').onclick=skin;
+document.getElementById('topRight').onclick=skinCare
+document.getElementById('bottomLeft').onclick=sunscreen
+document.getElementById('bottomMid').onclick=creme
+document.getElementById('bottomRight').onclick=secret
+function contact() {
+  var communication = window.open('https://www.nivea.in/about-us/contact')
+}
+function skin() {
+  var type = window.open('https://www.nivea.in/advice/nice-skin/what-skin-type-are-you-190')
+}
+function skinCare() {
+  var help = window.open('https://www.nivea.in/advice/nice-skin/what-skincare-type-are-you-160')
+}
+function sunscreen() {
+  var liquid = window.open('https://www.nivea.in/advice/sun-protection/the-right-sun-cream-102')
+}
+function creme() {
+  var different = window.open('https://www.nivea.in/shop/creams')
+}
+function secret() {
+  var discovery = window.open('https://www.nivea.in/advice/nice-skin/q10-q10plus-200')
+}
